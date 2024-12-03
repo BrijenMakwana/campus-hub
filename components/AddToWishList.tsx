3681506652095013c4f743345a231e7ab1614fe8
BrijenMakwana <brijenma@gmail.com>
2024-12-03 21:38:36 +0530

    code refactor

--- components/AddToWishList.tsx
+++ components/AddToWishList.tsx
@@ -1,11 +1,12 @@
-import Fontisto from '@expo/vector-icons/Fontisto';
-import { TouchableOpacity } from 'react-native';
+import { Button } from './ui/button';
+
+import { Bookmark } from '~/lib/icons/Bookmark';
 
 const AddToWishList = () => {
   return (
-    <TouchableOpacity className="absolute bottom-7 right-7 aspect-square w-16 items-center justify-center rounded-full border bg-background">
-      <Fontisto name="bookmark" size={30} color="black" />
-    </TouchableOpacity>
+    <Button className="absolute bottom-7 right-7  items-center justify-center rounded-full bg-yellow-400">
+      <Bookmark className="text-foreground" size={23} strokeWidth={1.25} />
+    </Button>
   );
 };
 
---
+++ lib/icons/BookmarkCheck.tsx
@@ -0,0 +1,5 @@
+import { BookmarkCheck } from 'lucide-react-native';
+
+import { iconWithClassName } from './iconWithClassName';
+iconWithClassName(BookmarkCheck);
+export { BookmarkCheck };
