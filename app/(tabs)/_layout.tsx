a80afb7f67b441ad89cf210ea16b5d6d5ef1e2ff
BrijenMakwana <brijenma@gmail.com>
2024-12-03 16:29:46 +0530

    tab bar icons

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -1,25 +1,27 @@
+import Fontisto from '@expo/vector-icons/Fontisto';
 import { Tabs } from 'expo-router';
-
-import { TabBarIcon } from '../../components/TabBarIcon';
 
 export default function TabLayout() {
   return (
     <Tabs
       screenOptions={{
         headerShown: false,
+        tabBarIconStyle: {
+          marginBottom: 1,
+        },
       }}>
       <Tabs.Screen
         name="index"
         options={{
-          title: 'Tab One',
-          tabBarIcon: ({ color }) => <TabBarIcon name="code" color={color} />,
+          title: 'Home',
+          tabBarIcon: ({ color }) => <Fontisto name="home" size={24} color={color} />,
         }}
       />
       <Tabs.Screen
         name="search-books"
         options={{
           title: 'Search',
-          tabBarIcon: ({ color }) => <TabBarIcon name="code" color={color} />,
+          tabBarIcon: ({ color }) => <Fontisto name="search" size={24} color={color} />,
         }}
       />
     </Tabs>
--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,17 +1,7 @@
-import { Stack } from 'expo-router';
 import { StyleSheet, View } from 'react-native';
 
-import { ScreenContent } from '~/components/ScreenContent';
-
 export default function Home() {
-  return (
-    <>
-      <Stack.Screen options={{ title: 'Tab One' }} />
-      <View style={styles.container}>
-        <ScreenContent path="app/(tabs)/index.tsx" title="Tab One" />
-      </View>
-    </>
-  );
+  return <View style={styles.container}></View>;
 }
 
 const styles = StyleSheet.create({
