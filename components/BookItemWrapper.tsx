5c89edcd5bc3ce6301a5bef6d7c1c0c20aaec59b
BrijenMakwana <brijenma@gmail.com>
2024-12-08 10:22:55 +0530

    ReanimatedSwipeable added to BookItemWrapper

--- components/BookItemWrapper.tsx
+++ components/BookItemWrapper.tsx
@@ -1,7 +1,13 @@
+import { View } from 'react-native';
+import { GestureHandlerRootView } from 'react-native-gesture-handler';
+import ReanimatedSwipeable from 'react-native-gesture-handler/ReanimatedSwipeable';
+import Reanimated, { SharedValue, useAnimatedStyle } from 'react-native-reanimated';
+
 import BookItem from './BookItem';
 
 import { Skeleton } from '~/components/ui/skeleton';
 import { useBook } from '~/hooks';
+import { Trash } from '~/lib/icons/Trash';
 import { IBookSale, IWishlistBook } from '~/types';
 
 const BookItemWrapper = (props: IWishlistBook | IBookSale) => {
@@ -13,7 +19,46 @@ const BookItemWrapper = (props: IWishlistBook | IBookSale) => {
 
   if (error) return;
 
-  return <BookItem {...book} {...rest} id={book_id} />;
+  return (
+    <GestureHandlerRootView>
+      <ReanimatedSwipeable
+        friction={2}
+        enableTrackpadTwoFingerGesture
+        rightThreshold={40}
+        renderRightActions={RightAction}>
+        <BookItem {...book} {...rest} id={book_id} />
+      </ReanimatedSwipeable>
+    </GestureHandlerRootView>
+  );
 };
 
 export default BookItemWrapper;
+
+function RightAction(prog: SharedValue<number>, drag: SharedValue<number>) {
+  const styleAnimation = useAnimatedStyle(() => {
+    return {
+      transform: [{ translateX: drag.value + 100 }],
+    };
+  });
+
+  return (
+    <Reanimated.View style={styleAnimation}>
+      <View
+        style={{
+          width: 100,
+          height: '100%',
+          alignItems: 'center',
+          justifyContent: 'center',
+        }}>
+        <Trash
+          className="text-secondary"
+          size={25}
+          strokeWidth={2}
+          style={{
+            width: 100,
+          }}
+        />
+      </View>
+    </Reanimated.View>
+  );
+}
