8e44310065a9de8ba88b903cc73e85d69718f215
BrijenMakwana <brijenma@gmail.com>
2024-12-10 09:57:36 +0530

    code refactor

--- hooks/index.ts
+++ hooks/index.ts
@@ -1,10 +1,9 @@
-export * from './useSearchBooks';
-export * from './useBook';
-export * from './useAnimatedHeader';
-export * from './useMyListedBooks';
-export * from './useWishListedBooks';
-export * from './useBookListing';
-export * from './useBookListings';
+export * from './queries/useSearchBooks';
+export * from './queries/useBook';
+export * from './queries/useMyListedBooks';
+export * from './queries/useWishListedBooks';
+export * from './queries/useBookListing';
+export * from './queries/useBookListings';
 
 export * from './mutations/useSignIn';
 export * from './mutations/useSignUp';
@@ -12,3 +11,5 @@ export * from './mutations/useListBookForSale';
 export * from './mutations/useAddBookToWishlist';
 export * from './mutations/useRemoveBookFromSale';
 export * from './mutations/useRemoveBookFromWishlist';
+
+export * from './useAnimatedHeader';
