5f07ad4f04b9751b413fa4a2a6be94a68725fcad
BrijenMakwana <brijenma@gmail.com>
2024-12-14 17:47:49 +0530

    app info screen

---
+++ app/app-info.tsx
@@ -0,0 +1,63 @@
+import EvilIcons from '@expo/vector-icons/EvilIcons';
+import FontAwesome from '@expo/vector-icons/FontAwesome';
+import FontAwesome5 from '@expo/vector-icons/FontAwesome5';
+import MaterialIcons from '@expo/vector-icons/MaterialIcons';
+import { TouchableOpacity, TouchableOpacityProps, View, Linking } from 'react-native';
+import { SafeAreaView } from 'react-native-safe-area-context';
+
+import GoBack from '~/components/GoBack';
+import { Separator } from '~/components/ui/separator';
+import { Text } from '~/components/ui/text';
+import { THEME } from '~/lib/constants';
+
+const AppInfoScreen = () => {
+  return (
+    <SafeAreaView className="flex-1 gap-5 bg-background p-5">
+      <GoBack text="Back" color="text-secondary" />
+
+      <View className="rounded-xl border border-input">
+        <ExternalButton
+          text="Source Code"
+          icon={<FontAwesome5 name="github" size={25} color={THEME.light.accent} />}
+          url=""
+        />
+        <Separator />
+        <ExternalButton
+          text="Privacy Policy"
+          icon={<MaterialIcons name="policy" size={25} color={THEME.light.accent} />}
+          url=""
+        />
+        <Separator />
+        <ExternalButton
+          text="Terms and Condition"
+          icon={<FontAwesome name="legal" size={25} color={THEME.light.accent} />}
+          url=""
+        />
+      </View>
+    </SafeAreaView>
+  );
+};
+
+export default AppInfoScreen;
+
+interface IExternalButton extends TouchableOpacityProps {
+  icon: React.ReactNode;
+  text: string;
+  url: string;
+}
+
+const ExternalButton = (props: IExternalButton) => {
+  const { icon, text, url } = props;
+
+  return (
+    <TouchableOpacity
+      className="flex flex-row items-center justify-between gap-5 px-5 py-3"
+      onPress={() => Linking.openURL(url)}>
+      {icon}
+
+      <Text className="flex-1 font-medium">{text}</Text>
+
+      <EvilIcons name="external-link" size={25} color="black" />
+    </TouchableOpacity>
+  );
+};
--- components/Header.tsx
+++ components/Header.tsx
@@ -1,10 +1,13 @@
+import { Link } from 'expo-router';
 import { View } from 'react-native';
 
 import Loading from './Loading';
+import { Button } from './ui/button';
 import { Separator } from './ui/separator';
 import { Text } from './ui/text';
 
 import { useCurrentUser } from '~/hooks';
+import { Info } from '~/lib/icons/Info';
 
 const Header = () => {
   const { data: user, isPending } = useCurrentUser();
@@ -12,12 +15,22 @@ const Header = () => {
   if (isPending) return <Loading />;
 
   return (
-    <View className="gap-3 px-5 pt-5">
-      <Text className="text-neutral-60 text-xl">Hello {user?.user_metadata.full_name}</Text>
-      <Text className="text-2xl">Explore, Connect, and Read!</Text>
+    <>
+      <View className="flex flex-row items-center justify-between gap-5 px-5 pt-5">
+        <View className="flex-1 gap-3">
+          <Text className="text-neutral-60 text-xl">Hello {user?.user_metadata.full_name}</Text>
+          <Text className="text-2xl">Explore, Connect, and Read!</Text>
+        </View>
 
-      <Separator />
-    </View>
+        <Link href="/app-info" asChild>
+          <Button variant="outline">
+            <Info className="text-foreground" size={23} strokeWidth={1.5} />
+          </Button>
+        </Link>
+      </View>
+
+      <Separator className="mt-3" />
+    </>
   );
 };
 
