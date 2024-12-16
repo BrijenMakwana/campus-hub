c877551095697bc6f51da051b23319dec6d414a4
BrijenMakwana <brijenma@gmail.com>
2024-12-16 16:34:51 +0530

    add to wishlist check in book screen

--- components/AddToWishList.tsx
+++ components/AddToWishList.tsx
@@ -1,16 +1,24 @@
 import { TouchableOpacity } from 'react-native';
 
-import { useAddBookToWishlist } from '~/hooks';
+import { useAddBookToWishlist, useIsBookInWishlist } from '~/hooks';
 import { Bookmark } from '~/lib/icons/Bookmark';
+import { BookmarkCheck } from '~/lib/icons/BookmarkCheck';
 
 const AddToWishList = ({ bookId }: { bookId: string }) => {
-  const { mutate: addBookToWishlist, isPending } = useAddBookToWishlist();
+  const { mutate: addBookToWishlist } = useAddBookToWishlist();
+
+  const { data: isInWishlist } = useIsBookInWishlist(bookId);
 
   return (
     <TouchableOpacity
       className="items-center justify-center p-3"
-      onPress={() => addBookToWishlist({ bookId })}>
-      <Bookmark className="text-background" size={27} strokeWidth={1.5} />
+      onPress={() => addBookToWishlist({ bookId })}
+      disabled={isInWishlist}>
+      {isInWishlist ? (
+        <BookmarkCheck className="text-background" size={27} strokeWidth={1.5} />
+      ) : (
+        <Bookmark className="text-background" size={27} strokeWidth={1.5} />
+      )}
     </TouchableOpacity>
   );
 };
