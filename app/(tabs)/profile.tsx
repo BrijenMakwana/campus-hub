d7f7015da9bf53fa65d13fb9c07005356f670f6b
BrijenMakwana <brijenma@gmail.com>
2024-12-16 11:22:28 +0530

    code refactor

--- app/(tabs)/profile.tsx
+++ app/(tabs)/profile.tsx
@@ -1,6 +1,8 @@
 import { View } from 'react-native';
 import { SafeAreaView } from 'react-native-safe-area-context';
 
+import StudentIcon from '../../assets/student.svg';
+
 import CurrencyPicker from '~/components/CurrencyPicker';
 import Error from '~/components/Error';
 import Loading from '~/components/Loading';
@@ -8,6 +10,8 @@ import { Button } from '~/components/ui/button';
 import { Separator } from '~/components/ui/separator';
 import { Text } from '~/components/ui/text';
 import { useCurrentUser, useLogout } from '~/hooks';
+import { Mail } from '~/lib/icons/Mail';
+import { Phone } from '~/lib/icons/Phone';
 
 const ProfileScreen = () => {
   const { data: user, isPending, error, refetch } = useCurrentUser();
@@ -20,10 +24,21 @@ const ProfileScreen = () => {
 
   return (
     <SafeAreaView className="flex-1 gap-5 bg-background p-5">
-      <View className="gap-2">
-        <Text className="text-2xl font-semibold capitalize">{user?.user_metadata.full_name}</Text>
-        <Text className="font-medium text-gray-500">{user?.email}</Text>
-        <Text className="font-medium text-gray-500">{user?.user_metadata.phone}</Text>
+      <View className="gap-3">
+        <View className="flex flex-row items-center gap-3">
+          <StudentIcon height={30} width={30} />
+          <Text className="text-2xl font-semibold capitalize">{user?.user_metadata.full_name}</Text>
+        </View>
+
+        <View className="flex flex-row items-center gap-3">
+          <Mail className="text-gray-500" size={20} strokeWidth={2} />
+          <Text className="font-medium text-gray-500">{user?.user_metadata.email}</Text>
+        </View>
+
+        <View className="flex flex-row items-center gap-3">
+          <Phone className="text-gray-500" size={20} strokeWidth={2} />
+          <Text className="font-medium text-gray-500">{user?.user_metadata.phone}</Text>
+        </View>
       </View>
 
       <Separator />
