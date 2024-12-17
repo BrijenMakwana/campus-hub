ff55b0742cc412d2fc57ee4763eb1450cecc48e3
BrijenMakwana <brijenma@gmail.com>
2024-12-17 10:09:53 +0530

    BookCard refactor

--- components/BookCard.tsx
+++ components/BookCard.tsx
@@ -1,20 +1,36 @@
+import dayjs from 'dayjs';
+import relativeTime from 'dayjs/plugin/relativeTime';
 import { Link } from 'expo-router';
 import { Image, TouchableOpacity, View } from 'react-native';
 
+import { Badge } from './ui/badge';
 import { Skeleton } from './ui/skeleton';
 import { Text } from './ui/text';
 
 import { useBook } from '~/hooks';
-import { IBookSale } from '~/types';
+import { cn } from '~/lib/utils';
+import useCurrencyStore from '~/store';
+import { IBookSale, BookCondition } from '~/types';
 
-const BookCard = (props: IBookSale) => {
-  const { book_id } = props;
+interface IBookCard extends IBookSale {
+  isPrice: boolean;
+  isBookCondition: boolean;
+  isCreatedAt: boolean;
+}
+
+const BookCard = (props: IBookCard) => {
+  const { book_id, price, book_condition, created_at, isPrice, isCreatedAt, isBookCondition } =
+    props;
 
   const { data: book, isPending } = useBook(book_id);
+
+  const { currency, getExchangeRate } = useCurrencyStore();
 
   if (isPending) return <Skeleton className="h-80 w-48 bg-neutral-300" />;
 
   const { imageLinks, title, authors } = book!.volumeInfo;
+
+  dayjs.extend(relativeTime);
 
   return (
     <Link href={`/book/${book_id}`} asChild>
@@ -30,9 +46,38 @@ const BookCard = (props: IBookSale) => {
           <Text className="font-semibold" numberOfLines={2}>
             {title}
           </Text>
-          <Text className="text-sm font-medium text-gray-500" numberOfLines={2}>
+          <Text className="text-sm font-medium text-gray-500" numberOfLines={1}>
             {authors.join(', ')}
           </Text>
+        </View>
+
+        <View className="mt-1">
+          {isPrice && (
+            <Text className="text-lg font-medium text-primary">
+              {currency.symbol}
+              {getExchangeRate(price)}
+            </Text>
+          )}
+
+          {isBookCondition && (
+            <Badge variant="outline" className="self-start">
+              <Text
+                className={cn(
+                  'text-sm font-medium',
+                  book_condition === BookCondition.GOOD
+                    ? 'text-green-400'
+                    : book_condition === BookCondition.USED
+                      ? 'text-yellow-400'
+                      : 'text-red-400'
+                )}>
+                {book_condition}
+              </Text>
+            </Badge>
+          )}
+
+          {isCreatedAt && (
+            <Text className="text-sm text-neutral-500">Added {dayjs(created_at).fromNow()}</Text>
+          )}
         </View>
       </TouchableOpacity>
     </Link>
