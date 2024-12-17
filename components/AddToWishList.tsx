3d3950a2dbf1b98ea409952e242c766ee3b15867
BrijenMakwana <brijenma@gmail.com>
2024-12-17 16:01:16 +0530

    code refactor

--- components/AddToWishList.tsx
+++ components/AddToWishList.tsx
@@ -5,16 +5,18 @@ import { Bookmark } from '~/lib/icons/Bookmark';
 import { BookmarkCheck } from '~/lib/icons/BookmarkCheck';
 
 const AddToWishList = ({ bookId }: { bookId: string }) => {
-  const { mutate: addBookToWishlist } = useAddBookToWishlist();
+  const { mutate: addBookToWishlist, isSuccess } = useAddBookToWishlist();
 
   const { data: isInWishlist } = useIsBookInWishlist(bookId);
+
+  const shouldShowCheckIcon = isInWishlist || isSuccess;
 
   return (
     <TouchableOpacity
       className="items-center justify-center p-3"
       onPress={() => addBookToWishlist({ bookId })}
-      disabled={isInWishlist}>
-      {isInWishlist ? (
+      disabled={shouldShowCheckIcon}>
+      {shouldShowCheckIcon ? (
         <BookmarkCheck className="text-background" size={27} strokeWidth={1.5} />
       ) : (
         <Bookmark className="text-background" size={27} strokeWidth={1.5} />
